0e1d62b8da99f53c3895cb2d971d60ab1cb936b2
Guilherme Nunes <guilhermego111nowww@gmail.com>
2023-10-09 21:30:40 -0300

    tirando DAO

--- src/main/java/controllers/LivroController.java
+++ src/main/java/controllers/LivroController.java
@@ -1,54 +1,122 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import DAOs.LivroDAO;
 import models.Livro;
+import persistence.BancoDados;
 
 public class LivroController {
+
+    private static final String SELECT_ALL_LIVROS = "SELECT * FROM livros";
+    private static final String SELECT_LIVRO_BY_GENERO = "SELECT * FROM livros WHERE genero = ?";
+    private static final String SELECT_LIVRO_BY_NOME = "SELECT * FROM livros WHERE nome = ?";
+    static Connection connection = null;
+
+    // Recebe todos os livros do banco de dados, a query passada é através da:
+    // private static final String SELECT_ALL_LIVROS
+    public static List<Livro> getAllLivros() {
+        List<Livro> livros = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = BancoDados.ConexaoDb();
+
+            preparedStatement = connection.prepareStatement(SELECT_ALL_LIVROS);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Livro livro = new Livro();
+                livro.setId(resultSet.getInt("id"));
+                livro.setNome(resultSet.getString("nome"));
+                livro.setSinopse(resultSet.getString("sinopse"));
+                livro.setGenero(resultSet.getString("genero"));
+                livro.setCapa(resultSet.getString("capa"));
+                livro.setArquivo_livro(resultSet.getString("arquivo_livro"));
+
+                livros.add(livro);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            BancoDados.fecharConexao(connection);
+        }
+
+        return livros;
+    }
+
+    public static Livro[] getLivroByGenero(String genero) {
+        List<Livro> livrosList = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
     
-    public static Livro[] getAllLivros() {
-        List<Livro> livrosList = LivroDAO.getAllLivros();
-
-        if (livrosList != null) {
-            Livro[] livrosArray = new Livro[livrosList.size()];
-            livrosArray = livrosList.toArray(livrosArray);
-
-            return livrosArray;
-        } else {
-            System.out.println("Falha ao conectar no banco de dados");
-            return null;
+        try {
+            connection = BancoDados.ConexaoDb();
+            preparedStatement = connection.prepareStatement(SELECT_LIVRO_BY_GENERO);
+            preparedStatement.setString(1, genero);
+    
+            resultSet = preparedStatement.executeQuery();
+    
+            while (resultSet.next()) {
+                Livro livro = new Livro();
+    
+                livro.setId(resultSet.getInt("id"));
+                livro.setNome(resultSet.getString("nome"));
+                livro.setSinopse(resultSet.getString("sinopse"));
+                livro.setGenero(resultSet.getString("genero"));
+                livro.setCapa(resultSet.getString("capa"));
+                livro.setArquivo_livro(resultSet.getString("arquivo_livro"));
+    
+                livrosList.add(livro);
+            }
+    
+        } catch (SQLException e) {
+            e.getErrorCode();
+        } finally {
+            BancoDados.fecharConexao(connection);
         }
+    
+        // Convert the list to an array and return it
+        return Arrays.toString(livrosList);
     }
+       
 
-    public static Livro getLivroById(int livroId) {
-        return LivroDAO.getLivroById(livroId);
-    }
+    public static List<Livro> getLivroByNome(String nomeLivro) {
+        List<Livro> livros = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
-    public static Livro[] getLivrosByGenero(String generoLivro) {
-        List<Livro> livroList = LivroDAO.getLivroByGenero(generoLivro);
+        try {
+            connection = BancoDados.ConexaoDb();
+            preparedStatement = connection.prepareStatement(SELECT_LIVRO_BY_NOME);
+            preparedStatement.setString(1, nomeLivro);
 
-        if (livroList != null) {
-            Livro[] livrosArray = new Livro[livroList.size()];
-            livrosArray = livroList.toArray(livrosArray);
+            resultSet = preparedStatement.executeQuery();
 
-            return livrosArray;
-        } else {
-            System.out.println("Falha ao conectar ao Banco de Dados");
-            return null;
+            while (resultSet.next()) {
+                Livro livro = new Livro();
+
+                livro.setId(resultSet.getInt("id"));
+                livro.setNome(resultSet.getString("nome"));
+                livro.setSinopse(resultSet.getString("sinopse"));
+                livro.setGenero(resultSet.getString("genero"));
+                livro.setCapa(resultSet.getString("capa"));
+                livro.setArquivo_livro(resultSet.getString("arquivo_livro"));
+
+                livros.add(livro);
+            }
+
+        } catch (SQLException e) {
+            e.getErrorCode();
+        } finally {
+            BancoDados.fecharConexao(connection);
         }
+
+        return livros;
     }
-
-    public static Livro[] getLivrosByNome(String nomeLivro) {
-        List<Livro> livroList = LivroDAO.getLivroByGenero(nomeLivro);
-
-        if (livroList != null) {
-            Livro[] livrosArray = new Livro[livroList.size()];
-            livrosArray = livroList.toArray(livrosArray);
-
-            return livrosArray;
-        } else {
-            System.out.println("Falha ao conectar ao Banco de Dados");
-            return null;
-        }
-    }    
 }
