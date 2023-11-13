995f65140c7aa0f5cc619853e09d0ef89822a4e2
Moises Rafael Franco - 823128231 <823128231@ulife.com.br>
2023-11-13 19:23:04 -0300

    Alterando getAllLivros

--- src/main/java/DAO/LivroDAO.java
+++ src/main/java/DAO/LivroDAO.java
@@ -11,18 +11,23 @@ import persistence.BancoDados;
 
 public class LivroDAO {
 
-    private static final String SELECT_ALL_LIVROS = "SELECT * FROM livro";
+    //private static final String SELECT_ALL_LIVROS = "SELECT * FROM livro";
     private static final String INSERT_LIVRO = "INSERT INTO livro(nome, sinopse, id_genero, autor) VALUES (?,?,?,?)";
-    private static final String GET_GENERO_BY_LIVRO_ID = "SELECT livro.*, g.* FROM livro INNER JOIN genero AS g ON livro.id_genero = g.id WHERE livro.id = ?";
-    private static final String GET_MEDIA_BY_LIVRO_ID = "SELECT " +
+    private static final String SELECT_ALL_LIVROS = "SELECT " +
+        "liv.id, " +
         "liv.nome, " +
-        "SUM(av.nota) AS soma_avaliacao, " +
-        "COUNT(av.id_livro) AS count_livro, " +
-        "SUM(av.nota) / COUNT(av.id_livro) AS media_avaliacao " +
-        "FROM public.avaliacao AS av " +
-        "INNER JOIN (SELECT id, nome FROM livro) AS liv ON liv.id = av.id_livro " +
-        "WHERE liv.id = ? " +
-        "GROUP BY av.id_usuario, av.id_livro, liv.nome;";
+        "liv.sinopse, " +
+        "liv.capa, " +
+        "liv.arquivo_livro, " +
+        "liv.id_genero, " +
+        "SUM(ava.nota) AS soma_avaliacao, " +
+        "COUNT(ava.id_livro) AS count_livro, " +
+        "ROUND(SUM(ava.nota) / CAST(COUNT(ava.id_livro) AS DECIMAL)) AS media_avaliacao, " +
+        "gen.nome_genero " +
+        "FROM livro AS liv " +
+        "JOIN genero AS gen ON gen.id = liv.id_genero " +
+        "JOIN avaliacao AS ava ON ava.id_livro = liv.id " +
+        "GROUP BY liv.id, liv.nome, gen.nome_genero;";
 
     // Recebe todos os livros do banco de dados, a query passada é através da:
     // private static final String SELECT_ALL_LIVROS
@@ -42,8 +47,8 @@ public class LivroDAO {
                 livro.setArquivo_livro(resultSet.getString("arquivo_livro"));
                 livro.setId_genero(resultSet.getInt("id_genero"));
                 
-                livro.setMedia(getMediaByLivroId(resultSet.getInt("id")));
-                livro.setGenero(getGeneroByLivroId(connection, resultSet.getInt("id")));
+                livro.setMedia(resultSet.getInt("media_avaliacao"));
+                livro.setGenero(resultSet.getString("nome_genero"));
 
                 livros.add(livro);
             }
@@ -53,27 +58,7 @@ public class LivroDAO {
     
         return livros;
     }
-    
-    public static String getGeneroByLivroId(Connection connection, int id) {
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        String genero = null;
-    
-        try {
-            preparedStatement = connection.prepareStatement(GET_GENERO_BY_LIVRO_ID);
-            preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
-    
-            if (resultSet.next()) {
-                genero = resultSet.getString("nome_genero");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    
-        return genero;
-    }
-            
+             
     //Insere os livros pedindo os campos como parâmetros obrigatorios
     public static int criarLivro(Livro livro) {
         int resultado = 0;
@@ -91,23 +76,5 @@ public class LivroDAO {
         System.out.println(resultado == 1 ? "Query realizada com sucesso" : "Houve um erro ao realizar a query");
         return resultado;
     }
-
-    //Realiza o cáculo de média de cada livro, baseado no id
-    //sum media / count media.
-    public static int getMediaByLivroId(int livroId) {
-        int media = 0;
-        try (Connection connection = BancoDados.ConexaoDb();
-            PreparedStatement preparedStatement = connection.prepareStatement(GET_MEDIA_BY_LIVRO_ID)) {
-            preparedStatement.setInt(1, livroId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                media = resultSet.getInt("media_avaliacao");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return media;
-    }
-
 
 }
