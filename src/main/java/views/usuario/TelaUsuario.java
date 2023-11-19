bd4707771d394320dda8d1e7c7705a50354fbc7d
Guilherme Nunes <guilhermego111nowww@gmail.com>
2023-11-19 00:14:21 -0300

    TelaPesquisar funcionando e outros ajustes

--- src/main/java/views/usuario/TelaUsuario.java
+++ src/main/java/views/usuario/TelaUsuario.java
@@ -47,7 +47,7 @@ public class TelaUsuario extends javax.swing.JFrame {
         jTextField5 = new javax.swing.JTextField();
         jTextField6 = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Nome:");
 
